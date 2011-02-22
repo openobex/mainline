openobex/mainline

commit b807401578a6dcd19e44d23f762c1e4461fde1d5

Fix miscellaneous coding style issues

lib/inobex.c
@@ -88,7 +88,7 @@ static int inobex_init (obex_t *self)
 		return -1;
 	}
 	if (LOBYTE(WSAData.wVersion) != WSA_VER_MAJOR ||
-	    HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
+				HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
 		DEBUG(4, "WSA version mismatch\n");
 		WSACleanup();
 		return -1;
@@ -107,30 +107,40 @@ static void inobex_cleanup (obex_t *self)
 
 static int inobex_set_remote_addr(obex_t *self, struct sockaddr *addr, size_t len)
 {
-	if ((addr->sa_family == AF_INET &&
-	     len == sizeof(struct sockaddr_in)) ||
-	    (addr->sa_family == AF_INET6 &&
-	     len == sizeof(struct sockaddr_in6)))
-	{
-		inobex_prepare_connect(self, addr, len);
-		return 0;
-	}
+	size_t expected_len;
 
-	return -1;
+	if (addr->sa_family == AF_INET)
+		expected_len = sizeof(struct sockaddr_in);
+	else if (addr->sa_family == AF_INET6)
+		expected_len = sizeof(struct sockaddr_in6);
+	else
+		return -1;
+
+	if (expected_len != len)
+		return -1;
+
+	inobex_prepare_connect(self, addr, len);
+
+	return 0;
 }
 
 static int inobex_set_local_addr(obex_t *self, struct sockaddr *addr, size_t len)
 {
-	if ((addr->sa_family == AF_INET &&
-	     len == sizeof(struct sockaddr_in)) ||
-	    (addr->sa_family == AF_INET6 &&
-	     len == sizeof(struct sockaddr_in6)))
-	{
-		inobex_prepare_listen(self, addr, len);
-		return 0;
-	}
+	size_t expected_len;
 
-	return -1;
+	if (addr->sa_family == AF_INET)
+		expected_len = sizeof(struct sockaddr_in);
+	else if (addr->sa_family == AF_INET6)
+		expected_len = sizeof(struct sockaddr_in6);
+	else
+		return -1;
+
+	if (expected_len != len)
+		return -1;
+
+	inobex_prepare_listen(self, addr, len);
+
+	return 0;
 }
 
 /*
@@ -217,8 +227,8 @@ static int inobex_listen(obex_t *self)
 	 * instead of InOBEX_TransportConnect (e.g. obexftp)
 	 */
 	if (data->self.sin6_family == AF_INET)
-		inobex_prepare_listen(self, (struct sockaddr*)&data->self,
-				       sizeof(data->self));
+		inobex_prepare_listen(self, (struct sockaddr *) &data->self,
+							sizeof(data->self));
 
 	trans->serverfd = obex_create_socket(self, AF_INET6);
 	if (trans->serverfd == INVALID_SOCKET) {
@@ -233,15 +243,15 @@ static int inobex_listen(obex_t *self)
 		 * You will certainly notice later if it failed.
 		 */
 		int v6only = 0;
-		(void)setsockopt(trans->serverfd, IPPROTO_IPV6,
-				 IPV6_V6ONLY, (void*)&v6only, sizeof(v6only));
+		setsockopt(trans->serverfd, IPPROTO_IPV6, IPV6_V6ONLY,
+					(void *) &v6only, sizeof(v6only));
 	}
 #endif
 
 	//printf("TCP/IP listen %d %X\n", trans->self.inet.sin_port,
 	//       trans->self.inet.sin_addr.s_addr);
 	if (bind(trans->serverfd, (struct sockaddr *) &data->self,
-		 sizeof(data->self))) {
+							sizeof(data->self))) {
 		DEBUG(0, "bind() Failed\n");
 		return -1;
 	}
@@ -251,7 +261,8 @@ static int inobex_listen(obex_t *self)
 		return -1;
 	}
 
-	DEBUG(4, "Now listening for incomming connections. serverfd = %d\n", trans->serverfd);
+	DEBUG(4, "Now listening for incomming connections. serverfd = %d\n",
+							trans->serverfd);
 
 	return 1;
 }
@@ -300,8 +311,8 @@ static int inobex_connect_request(obex_t *self)
 	 * instead of InOBEX_TransportConnect (e.g. obexftp)
 	 */
 	if (data->peer.sin6_family == AF_INET)
-		inobex_prepare_connect(self, (struct sockaddr*)&data->peer,
-				       sizeof(data->peer));
+		inobex_prepare_connect(self, (struct sockaddr *) &data->peer,
+							sizeof(data->peer));
 
 	trans->fd = obex_create_socket(self, AF_INET6);
 	if (trans->fd == INVALID_SOCKET)
@@ -314,8 +325,8 @@ static int inobex_connect_request(obex_t *self)
 		 * You will certainly notice later if it failed.
 		 */
 		int v6only = 0;
-		(void)setsockopt(trans->fd, IPPROTO_IPV6, IPV6_V6ONLY,
-				 (void*)&v6only, sizeof(v6only));
+		setsockopt(trans->fd, IPPROTO_IPV6, IPV6_V6ONLY,
+						&v6only, sizeof(v6only));
 	}
 #endif
 
@@ -334,7 +345,7 @@ static int inobex_connect_request(obex_t *self)
 #endif
 
 	ret = connect(trans->fd, (struct sockaddr *) &data->peer,
-		      sizeof(data->peer));
+							sizeof(data->peer));
 	if (ret == -1) {
 		DEBUG(4, "Connect failed\n");
 		obex_delete_socket(self, trans->fd);

lib/usbobex.c
@@ -67,14 +67,15 @@ static int usbobex_select_interface(obex_t *self, obex_interface_t *intf)
  * Helper function to usbobex_find_interfaces
  */
 static void find_eps(struct obex_usb_intf_transport_t *intf,
-		     struct usb_interface_descriptor data_intf,
-		     int *found_active, int *found_idle)
+			struct usb_interface_descriptor data_intf,
+			int *found_active, int *found_idle)
 {
-	struct usb_endpoint_descriptor *ep0, *ep1;
-
 	if (data_intf.bNumEndpoints == 2) {
+		struct usb_endpoint_descriptor *ep0, *ep1;
+
 		ep0 = data_intf.endpoint;
 		ep1 = data_intf.endpoint + 1;
+
 		if ((ep0->bEndpointAddress & USB_ENDPOINT_IN) &&
 		    ((ep0->bmAttributes & USB_ENDPOINT_TYPE_MASK) == USB_ENDPOINT_TYPE_BULK) &&
 		    !(ep1->bEndpointAddress & USB_ENDPOINT_IN) &&
@@ -96,6 +97,7 @@ static void find_eps(struct obex_usb_intf_transport_t *intf,
 			intf->data_endpoint_write = ep0->bEndpointAddress;
 		}
 	}
+
 	if (data_intf.bNumEndpoints == 0) {
 		*found_idle = 1;
 		intf->data_idle_setting = data_intf.bAlternateSetting;
@@ -107,8 +109,8 @@ static void find_eps(struct obex_usb_intf_transport_t *intf,
  * Helper function to usbobex_find_interfaces
  */
 static int find_obex_data_interface(unsigned char *buffer, int buflen,
-				    struct usb_config_descriptor config,
-				    struct obex_usb_intf_transport_t *intf)
+					struct usb_config_descriptor config,
+					struct obex_usb_intf_transport_t *intf)
 {
 	struct cdc_union_desc *union_header = NULL;
 	int i, a;
@@ -177,8 +179,8 @@ next_desc:
 /*
  * Helper function to usbobex_find_interfaces
  */
-static int get_intf_string(struct usb_dev_handle *usb_handle, char **string,
-			   int id)
+static int get_intf_string(struct usb_dev_handle *usb_handle,
+							char **string, int id)
 {
 	if (id) {
 		*string = malloc(USB_MAX_STRING_SIZE);
@@ -195,16 +197,19 @@ static int get_intf_string(struct usb_dev_handle *usb_handle, char **string,
  * Helper function to usbobex_find_interfaces
  */
 static struct obex_usb_intf_transport_t *check_intf(struct usb_device *dev,
-				      int c, int i, int a,
-				      struct obex_usb_intf_transport_t *current)
+					int c, int i, int a,
+					struct obex_usb_intf_transport_t *current)
 {
 	struct obex_usb_intf_transport_t *next = NULL;
+	struct usb_interface_descriptor *alt;
 
-	if ((dev->config[c].interface[i].altsetting[a].bInterfaceClass == USB_CDC_CLASS)
-	    && (dev->config[c].interface[i].altsetting[a].bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS)) {
+	alt = &dev->config[c].interface[i].altsetting[a];
+
+	if (alt->bInterfaceClass == USB_CDC_CLASS) &&
+			(alt->bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS) {
 		int err;
-		unsigned char *buffer = dev->config[c].interface[i].altsetting[a].extra;
-		int buflen = dev->config[c].interface[i].altsetting[a].extralen;
+		unsigned char *buffer = alt->extra;
+		int buflen = alt->extralen;
 
 		next = malloc(sizeof(*next));
 		if (next == NULL)
@@ -212,9 +217,9 @@ static struct obex_usb_intf_transport_t *check_intf(struct usb_device *dev,
 		next->device = dev;
 		next->configuration = dev->config[c].bConfigurationValue;
 		next->configuration_description = dev->config[c].iConfiguration;
-		next->control_interface = dev->config[c].interface[i].altsetting[a].bInterfaceNumber;
-		next->control_interface_description = dev->config[c].interface[i].altsetting[a].iInterface;
-		next->control_setting = dev->config[c].interface[i].altsetting[a].bAlternateSetting;
+		next->control_interface = alt->bInterfaceNumber;
+		next->control_interface_description = alt->iInterface;
+		next->control_setting = alt->bAlternateSetting;
 		next->extra_descriptors = malloc(buflen);
 		if (next->extra_descriptors)
 			memcpy(next->extra_descriptors, buffer, buflen);
@@ -475,6 +480,7 @@ static int usbobex_write(obex_t *self, buf_t *msg)
 		errno = -status;
 		return -1;
 	}
+
 	return status;
 }
 
@@ -490,9 +496,9 @@ static int usbobex_read(obex_t *self, void *buf, int buflen)
 	/* USB can only read 0xFFFF bytes at once (equals mtu_rx) */
 	DEBUG(4, "Endpoint %d\n", data->self.data_endpoint_read);
 	status = usb_bulk_read(data->self.dev,
-			       data->self.data_endpoint_read,
-			       buf, self->mtu_rx,
-			       usbobex_get_timeout(trans->timeout));
+				data->self.data_endpoint_read,
+				buf, self->mtu_rx,
+				usbobex_get_timeout(trans->timeout));
 
 	if (status < 0) {
 		if (status == -ETIMEDOUT)
@@ -523,6 +529,6 @@ void usbobex_get_ops(struct obex_transport_ops* ops)
 	ops->client.find_interfaces = &usbobex_find_interfaces;
 	ops->client.free_interface = &usbobex_free_interface;
 	ops->client.select_interface = &usbobex_select_interface;
-};
+}
 
 #endif /* HAVE_USB */

lib/btobex.c
@@ -50,7 +50,7 @@ bdaddr_t bluez_compat_bdaddr_any = { BTH_ADDR_NULL };
 
 #include "cloexec.h"
 
-static int btobex_init (obex_t *self)
+static int btobex_init(obex_t *self)
 {
 #ifdef _WIN32
 	WORD ver = MAKEWORD(WSA_VER_MAJOR,WSA_VER_MINOR);
@@ -60,7 +60,7 @@ static int btobex_init (obex_t *self)
 		return -1;
 	}
 	if (LOBYTE(WSAData.wVersion) != WSA_VER_MAJOR ||
-	    HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
+				HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
 		DEBUG(4, "WSA version mismatch\n");
 		WSACleanup();
 		return -1;
@@ -77,10 +77,11 @@ static void btobex_cleanup (obex_t *self)
 #endif
 }
 
-static int btobex_set_local_addr(obex_t *self, struct sockaddr *addr, size_t len)
+static int btobex_set_local_addr(obex_t *self, struct sockaddr *addr,
+								size_t len)
 {
 	struct btobex_data *data = &self->trans.data.rfcomm;
-	const struct sockaddr_rc *local = (struct sockaddr_rc *)addr;
+	const struct sockaddr_rc *local = (struct sockaddr_rc *) addr;
 
 	if (len == sizeof(*local) && local->rc_family == AF_BLUETOOTH) {
 		data->self = *local;
@@ -110,7 +111,7 @@ static int btobex_set_remote_addr(obex_t *self, struct sockaddr *addr, size_t le
  *
  */
 void btobex_prepare_connect(obex_t *self, const bdaddr_t *src,
-			    const bdaddr_t *dst, uint8_t channel)
+					const bdaddr_t *dst, uint8_t channel)
 {
 	struct sockaddr_rc addr;
 
@@ -119,7 +120,7 @@ void btobex_prepare_connect(obex_t *self, const bdaddr_t *src,
 	addr.rc_family = AF_BLUETOOTH;
 	bacpy(&addr.rc_bdaddr, dst);
 	addr.rc_channel = channel;
-	btobex_set_remote_addr(self, (struct sockaddr *)&addr, sizeof(addr));
+	btobex_set_remote_addr(self, (struct sockaddr *) &addr, sizeof(addr));
 }
 
 /*
@@ -135,7 +136,7 @@ void btobex_prepare_listen(obex_t *self, const bdaddr_t *src, uint8_t channel)
 	addr.rc_family = AF_BLUETOOTH;
 	bacpy(&addr.rc_bdaddr, src);
 	addr.rc_channel = channel;
-	btobex_set_local_addr(self, (struct sockaddr *)&addr, sizeof(addr));
+	btobex_set_local_addr(self, (struct sockaddr *) &addr, sizeof(addr));
 }
 
 /*
@@ -157,8 +158,8 @@ static int btobex_listen(obex_t *self)
 		return -1;
 	}
 
-	if (bind(trans->serverfd, (struct sockaddr*) &data->self,
-		 sizeof(data->self))) {
+	if (bind(trans->serverfd, (struct sockaddr *) &data->self,
+							sizeof(data->self))) {
 		DEBUG(0, "Error doing bind\n");
 		goto out_freesock;
 	}
@@ -190,7 +191,7 @@ static int btobex_accept(obex_t *self)
 {
 	struct obex_transport *trans = &self->trans;
 	struct btobex_data *data = &self->trans.data.rfcomm;
-	struct sockaddr *addr = (struct sockaddr *)&data->peer;
+	struct sockaddr *addr = (struct sockaddr *) &data->peer;
 	socklen_t addrlen = sizeof(data->peer);
 
 	// First accept the connection and get the new client socket.
@@ -229,7 +230,7 @@ static int btobex_connect_request(obex_t *self)
 	}
 
 	ret = bind(trans->fd, (struct sockaddr*) &data->self,
-		   sizeof(data->self));
+							sizeof(data->self));
 
 	if (ret < 0) {
 		DEBUG(4, "bind(): error %d\n", errno);
@@ -237,8 +238,8 @@ static int btobex_connect_request(obex_t *self)
 	}
 
 	ret = connect(trans->fd, (struct sockaddr*) &data->peer,
-		      sizeof(data->peer));
-	if (ret == -1) {
+							sizeof(data->peer));
+	if (ret < 0) {
 		DEBUG(4, "connect(): error %d\n", errno);
 		goto out_freesock;
 	}
@@ -268,10 +269,13 @@ static int btobex_disconnect_request(obex_t *self)
 	int ret;
 
 	DEBUG(4, "\n");
+
 	ret = obex_delete_socket(self, trans->fd);
 	if (ret < 0)
 		return ret;
+
 	trans->fd = INVALID_SOCKET;
+
 	return ret;
 }
 
@@ -289,8 +293,10 @@ static int btobex_disconnect_server(obex_t *self)
 	int ret;
 
 	DEBUG(4, "\n");
+
 	ret = obex_delete_socket(self, trans->serverfd);
 	trans->serverfd = INVALID_SOCKET;
+
 	return ret;
 }
 
@@ -307,6 +313,6 @@ void btobex_get_ops(struct obex_transport_ops *ops)
 	ops->server.disconnect = &btobex_disconnect_server;
 	ops->client.connect = &btobex_connect_request;
 	ops->client.disconnect = &btobex_disconnect_request;
-};
+}
 
 #endif /* HAVE_BLUETOOTH */

lib/customtrans.c
@@ -11,7 +11,7 @@ static int custom_clone(obex_t *self, const obex_t *from)
 
 	*ctrans = *old;
 
-	return 0;	
+	return 0;
 }
 
 void custom_set_data(obex_t *self, void *data)

lib/fdobex.c
@@ -11,7 +11,7 @@
 #endif
 #endif
 
-static int fdobex_init (obex_t *self)
+static int fdobex_init(obex_t *self)
 {
 	self->trans.data.fd.writefd = INVALID_SOCKET;
 
@@ -24,7 +24,7 @@ static int fdobex_connect_request(obex_t *self)
 
 	/* no real connect on the file */
 	if (trans->fd != INVALID_SOCKET &&
-	    trans->data.fd.writefd != INVALID_SOCKET)
+				trans->data.fd.writefd != INVALID_SOCKET)
 		return 0;
 	else {
 		errno = EINVAL;
@@ -74,7 +74,7 @@ static int fdobex_write(obex_t *self, buf_t *msg)
 #endif
 }
 
-static int fdobex_read (obex_t *self, void *buf, int buflen)
+static int fdobex_read(obex_t *self, void *buf, int buflen)
 {
 	struct obex_transport *trans = &self->trans;
 
@@ -92,4 +92,4 @@ void fdobex_get_ops(struct obex_transport_ops* ops)
 	ops->read = &fdobex_read;
 	ops->client.connect = &fdobex_connect_request;
 	ops->client.disconnect = &fdobex_disconnect_request;
-};
+}

lib/irobex.c
@@ -70,7 +70,7 @@ static int irobex_init (obex_t *self)
 		return -1;
 	}
 	if (LOBYTE(WSAData.wVersion) != WSA_VER_MAJOR ||
-	    HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
+				HIBYTE(WSAData.wVersion) != WSA_VER_MINOR) {
 		DEBUG(4, "WSA version mismatch\n");
 		WSACleanup();
 		return -1;
@@ -258,7 +258,7 @@ static void irobex_set_hint_bit(obex_t *self)
 
 #ifndef _WIN32
 	/* Hint be we advertise */
-	unsigned char hints[4] = { 
+	unsigned char hints[4] = {
 		HINT_EXTENSION, HINT_OBEX, 0, 0,
 	};
 
@@ -266,8 +266,8 @@ static void irobex_set_hint_bit(obex_t *self)
 	 * This command is not supported by older kernels,
 	 * so ignore any errors!
 	 */
-	(void)setsockopt(trans->serverfd, SOL_IRLMP, IRLMP_HINTS_SET,
-			 hints, sizeof(hints));
+	setsockopt(trans->serverfd, SOL_IRLMP, IRLMP_HINTS_SET,
+							hints, sizeof(hints));
 #else /* _WIN32 */
 	/* The registry must be changed to set the hint bit. */
 #endif /* _WIN32 */
@@ -324,17 +324,17 @@ static unsigned int irobex_get_mtu(obex_t *self)
 
 	/* Check what the IrLAP data size is */
 	if (getsockopt(trans->fd, SOL_IRLMP, IRTTP_MAX_SDU_SIZE,
-		       (void *) &mtu, &len))
+							(void *) &mtu, &len))
 		return 0;
 #else
 	DWORD mtu;
 	int len = sizeof(mtu);
 
 	if (getsockopt(trans->fd, SOL_IRLMP, IRLMP_SEND_PDU_LEN,
-		       (char *) &mtu, &len))
+							(char *) &mtu, &len))
 		return 0;
 #endif /* _WIN32 */
-	return (int)mtu;
+	return (int) mtu;
 }
 
 /*
@@ -386,6 +386,8 @@ static int irobex_find_interfaces(obex_t *self, obex_interface_t **interfaces)
 	socklen_t len = sizeof(buf);
 	int count = 0;
 	int fd = obex_create_socket(self, AF_IRDA);
+	int i;
+	uint32_t k = 0;
 
 	if (fd == INVALID_SOCKET)
 		goto out;
@@ -398,11 +400,10 @@ static int irobex_find_interfaces(obex_t *self, obex_interface_t **interfaces)
 		};
 		int err;
 
-
 		/* Set the filter used for performing discovery */
 		err = setsockopt(fd, SOL_IRLMP, IRLMP_HINT_MASK_SET,
-				 hints, sizeof(hints));
-		if (err == -1) {
+							hints, sizeof(hints));
+		if (err < 0) {
 			perror("setsockopt");
 			goto out;
 		}
@@ -410,69 +411,72 @@ static int irobex_find_interfaces(obex_t *self, obex_interface_t **interfaces)
 #endif
 
 	/* Perform a discovery and get device list */
-	if (getsockopt(fd, SOL_IRLMP, IRLMP_ENUMDEVICES, (char *)buf, &len)) {
+	if (getsockopt(fd, SOL_IRLMP, IRLMP_ENUMDEVICES, (char *) buf, &len)) {
 		DEBUG(1, "Didn't find any devices!\n");
 		return 0;
 	}
 
-	list = (struct irda_device_list *)buf;
+	list = (struct irda_device_list *) buf;
 #ifndef _WIN32
-	count = (int)list->len;
+	count = (int) list->len;
 	dev = list->dev;
 #else
-	count = (int)list->numDevice;
+	count = (int) list->numDevice;
 	dev = list->Device;
 #endif
-	if (count > 0) {
-		int i = 0;
-		uint32_t k = 0;
-		*interfaces = calloc(count, sizeof(**interfaces));
+	if (count <= 0)
+		goto done;
 
-		DEBUG(1, "Discovered %u devices:\n", count);
-		for (; i < count; ++i) {
-			obex_irda_intf_t *intf = &((*interfaces)+k)->irda;
+	*interfaces = calloc(count, sizeof(**interfaces));
+
+	DEBUG(1, "Discovered %u devices:\n", count);
+	for (i = 0; i < count; ++i) {
+		obex_irda_intf_t *intf = &((*interfaces)+k)->irda;
 
 #ifndef _WIN32
-			intf->local = dev[i].saddr;
-			intf->remote = dev[i].daddr;
-			intf->charset = dev[i].charset;
-			/* allocate enough space to make sure the string is
-			 * zero-terminated
-			 */
-			intf->info = calloc(sizeof(dev[i].info)+2, 1);
-			if (intf->info)
-				memcpy(intf->info, dev[i].info, sizeof(dev[i].info));
-			intf->hints[0] = dev[i].hints[0];
-			intf->hints[1] = dev[i].hints[1];
+		intf->local = dev[i].saddr;
+		intf->remote = dev[i].daddr;
+		intf->charset = dev[i].charset;
+		/* allocate enough space to make sure the string is
+		 * zero-terminated
+		 */
+		intf->info = calloc(sizeof(dev[i].info)+2, 1);
+		if (intf->info)
+			memcpy(intf->info, dev[i].info, sizeof(dev[i].info));
+
+		intf->hints[0] = dev[i].hints[0];
+		intf->hints[1] = dev[i].hints[1];
 #else
-			if (self->init_flags & OBEX_FL_FILTERHINT &&
-			    ((dev[i].irdaDeviceHints1 & LM_HB_Extension) == 0 ||
-			     (dev[i].irdaDeviceHints2 & 0x20) == 0))
-				continue;
+		if ((self->init_flags & OBEX_FL_FILTERHINT) &&
+				((dev[i].irdaDeviceHints1 & LM_HB_Extension) == 0))
+			continue;
 
-			intf->remote = dev[i].irdaDeviceID[3]
-				| dev[i].irdaDeviceID[2] << 8
-				| dev[i].irdaDeviceID[1] << 16
-				| dev[i].irdaDeviceID[0] << 24;
-			intf->charset = dev[i].irdaCharSet;
-			/* allocate enough space to make sure the
-			 * string is zero-terminated */
-			intf->info = calloc(sizeof(dev[i].irdaDeviceName)+2, 1);
-			if (intf->info)
-				memcpy(intf->info, dev[i].irdaDeviceName,
-				       sizeof(dev[i].irdaDeviceName));
-			intf->hints[0] = dev[i].irdaDeviceHints1;
-			intf->hints[1] = dev[i].irdaDeviceHints2;
+		if ((dev[i].irdaDeviceHints2 & 0x20) == 0)
+			continue;
+
+		intf->remote = dev[i].irdaDeviceID[3]
+					| dev[i].irdaDeviceID[2] << 8
+					| dev[i].irdaDeviceID[1] << 16
+					| dev[i].irdaDeviceID[0] << 24;
+		intf->charset = dev[i].irdaCharSet;
+		/* allocate enough space to make sure the
+		 * string is zero-terminated */
+		intf->info = calloc(sizeof(dev[i].irdaDeviceName)+2, 1);
+		if (intf->info)
+			memcpy(intf->info, dev[i].irdaDeviceName,
+						sizeof(dev[i].irdaDeviceName));
+		intf->hints[0] = dev[i].irdaDeviceHints1;
+		intf->hints[1] = dev[i].irdaDeviceHints2;
 #endif
-			++k;
-			DEBUG(1, "  [%d] daddr: 0x%08x\n", i+1, intf->remote);
-		}
-		count = k;
+		++k;
+		DEBUG(1, "  [%d] daddr: 0x%08x\n", i+1, intf->remote);
 	}
 
-	if (count == 0) {
+	count = k;
+
+done:
+	if (count == 0)
 		DEBUG(1, "didn't find any OBEX devices!\n");
-	}
 
 out:
 	obex_delete_socket(self, fd);
@@ -514,7 +518,7 @@ static int irobex_connect_request(obex_t *self)
 	}
 
 	ret = connect(trans->fd, (struct sockaddr*) &data->peer,
-		      sizeof(data->peer));
+							sizeof(data->peer));
 	if (ret < 0) {
 		DEBUG(4, "ret=%d\n", ret);
 		goto out_freesock;

lib/obex_transport.c
@@ -119,7 +119,7 @@ void obex_transport_cleanup(obex_t *self)
 	obex_transport_disconnect_request(self);
 	obex_transport_disconnect_server(self);
 	if (self->trans.ops.cleanup)
-		self->trans.ops.cleanup(self);	
+		self->trans.ops.cleanup(self);
 }
 
 void obex_transport_clone(obex_t *self, obex_t *old)
@@ -435,14 +435,16 @@ int obex_transport_read(obex_t *self, int max)
 
 void obex_transport_enumerate(struct obex *self)
 {
-	int i = 0;
+	struct obex_transport_ops *ops = &self->trans.ops;
+	int i;
 
 	if (self->interfaces)
 		return;
 
-	if (self->trans.ops.client.find_interfaces)
-		i = self->trans.ops.client.find_interfaces(self,
-							&self->interfaces);
+	if (ops->client.find_interfaces)
+		i = ops->client.find_interfaces(self, &self->interfaces);
+	else
+		i = 0;
 
 	self->interfaces_number = i;
 }

lib/usb1obex.c
@@ -67,7 +67,8 @@ static int usbobex_init (obex_t *self)
 	}
 
 	libusb_set_pollfd_notifiers(data->ctx, &usbobex_set_fd,
-				    &usbobex_clear_fd, self);
+						&usbobex_clear_fd, self);
+
 	return 0;
 }
 
@@ -228,11 +229,15 @@ static struct obex_usb_intf_transport_t *check_intf(struct libusb_device *dev,
 					struct obex_usb_intf_transport_t *current)
 {
 	struct obex_usb_intf_transport_t *next = NULL;
-	if ((conf_desc->interface[i].altsetting[a].bInterfaceClass == USB_CDC_CLASS)
-	    && (conf_desc->interface[i].altsetting[a].bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS)) {
+	const struct libusb_interface_descriptor *alt;
+
+	alt = &conf_desc->interface[i].altsetting[a];
+
+	if (alt->bInterfaceClass == USB_CDC_CLASS &&
+			alt->bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS) {
 		int err;
-		const unsigned char *buffer = conf_desc->interface[i].altsetting[a].extra;
-		int buflen = conf_desc->interface[i].altsetting[a].extra_length;
+		const unsigned char *buffer = alt->extra;
+		int buflen = alt->extra_length;
 
 		next = malloc(sizeof(*next));
 		if (next == NULL)
@@ -241,9 +246,9 @@ static struct obex_usb_intf_transport_t *check_intf(struct libusb_device *dev,
 		libusb_ref_device(dev);
 		next->configuration = conf_desc->bConfigurationValue;
 		next->configuration_description = conf_desc->iConfiguration;
-		next->control_interface = conf_desc->interface[i].altsetting[a].bInterfaceNumber;
-		next->control_interface_description = conf_desc->interface[i].altsetting[a].iInterface;
-		next->control_setting = conf_desc->interface[i].altsetting[a].bAlternateSetting;
+		next->control_interface = alt->bInterfaceNumber;
+		next->control_interface_description = alt->iInterface;
+		next->control_setting = alt->bAlternateSetting;
 		next->extra_descriptors = malloc(buflen);
 		if (next->extra_descriptors)
 			memcpy(next->extra_descriptors, buffer, buflen);
@@ -260,6 +265,7 @@ static struct obex_usb_intf_transport_t *check_intf(struct libusb_device *dev,
 			current = next;
 		}
 	}
+
 	return current;
 }
 

commit 10b69ce470e33393b921ac4298774b8cfaa10f19

Fix Windows build broken by commit b807401578a6

lib/inobex.c
@@ -326,7 +326,7 @@ static int inobex_connect_request(obex_t *self)
 		 */
 		int v6only = 0;
 		setsockopt(trans->fd, IPPROTO_IPV6, IPV6_V6ONLY,
-						&v6only, sizeof(v6only));
+					(void *) &v6only, sizeof(v6only));
 	}
 #endif
 

lib/usbobex.c
@@ -205,8 +205,8 @@ static struct obex_usb_intf_transport_t *check_intf(struct usb_device *dev,
 
 	alt = &dev->config[c].interface[i].altsetting[a];
 
-	if (alt->bInterfaceClass == USB_CDC_CLASS) &&
-			(alt->bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS) {
+	if ((alt->bInterfaceClass == USB_CDC_CLASS) &&
+			(alt->bInterfaceSubClass == USB_CDC_OBEX_SUBCLASS)) {
 		int err;
 		unsigned char *buffer = alt->extra;
 		int buflen = alt->extralen;