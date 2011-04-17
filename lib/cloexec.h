2d7a36851a2ecf73822503ee695719107be21105
Hendrik Sattler <post@hendrik-sattler.de>
2011-04-17 22:27:24 +0200

    Add missing copyright and license plates

--- lib/cloexec.h
+++ lib/cloexec.h
@@ -1,3 +1,23 @@
+/**
+	\file cloexec.h
+	close-on-exec wrapper functions.
+	OpenOBEX library - Free implementation of the Object Exchange protocol.
+
+	Copyright (c) 2010 Hendrik Sattler, All Rights Reserved.
+
+	OpenOBEX is free software; you can redistribute it and/or modify
+	it under the terms of the GNU Lesser General Public License as
+	published by the Free Software Foundation; either version 2.1 of
+	the License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Lesser General Public License for more details.
+
+	You should have received a copy of the GNU Lesser General Public
+	License along with OpenOBEX. If not, see <http://www.gnu.org/>.
+ */
 
 #ifndef _WIN32
 #include <fcntl.h>
