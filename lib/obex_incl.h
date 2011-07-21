6b8b190f336a0a7d562dbf6a6520e535657e7df3
Hendrik Sattler <post@hendrik-sattler.de>
2011-07-21 22:04:55 +0200

    Make MSVC 7.1 compile again

--- lib/obex_incl.h
+++ lib/obex_incl.h
@@ -6,6 +6,12 @@
 
 /* This overides the define in openobex/obex.h */
 #define OPENOBEX_SYMBOL(retval) LIB_SYMBOL retval CALLAPI
+
+/* Visual Studio C++ Compiler 7.1 does not know about Bluetooth */
+#if defined(_MSC_VER) && _MSC_VER < 1400
+#define bt_addr_t void
+#endif
+
 #include <openobex/obex.h>
 
 #endif
