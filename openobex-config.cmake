6244eaa062405f7a6df239e4fcbed029ffc7fc0c
Hendrik Sattler <post@hendrik-sattler.de>
2012-06-16 08:37:01 +0200

    Extend cmake installed config file
    Additionally, the same file can be used to directly use the files from a
    build tree.

--- openobex-config.cmake
+++
@@ -1,7 +0,0 @@
-get_filename_component(SELF_DIR "${CMAKE_CURRENT_LIST_FILE}" PATH) # ...../lib/cmake/OpenObex-x.y/
-include(${SELF_DIR}/openobex-targets.cmake)
-
-get_filename_component(INSTALL_DIR "${SELF_DIR}" PATH)    # ...../lib/cmake/
-get_filename_component(INSTALL_DIR "${INSTALL_DIR}" PATH) # ...../lib/
-get_filename_component(INSTALL_DIR "${INSTALL_DIR}" PATH) # .....
-set(OpenObex_INCLUDE_DIRS "${INSTALL_DIR}/include/openobex")
